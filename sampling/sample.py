1d46041d7da2480e594481c7077d124e711e4594
YoannDupont <yoa.dupont@gmail.com>
2022-09-28 15:40:59 +0200

    New features
    # added
    
    - two options to have a minimal and maximal sentence length.
    - an option to give a list of id that should not be samples (creating a new sample from an already sampled corpus).

--- sampling/sample.py
+++ sampling/sample.py
@@ -28,10 +28,26 @@ def guess_format(pathname):
     raise ValueError("Unhandled file format: {}".format(pathname.suffix))
 
 
-def main(infilename, corpus_format="guess", sample_size=1000, output_dir="."):
+def main(
+    infilename,
+    corpus_format="guess",
+    sample_size=1000,
+    min_size=5,
+    max_size=60,
+    output_dir=".",
+    do_not_sample=None,
+):
     infilepath = Path(infilename)
     if corpus_format == "guess":
         corpus_format = guess_format(infilepath)
+
+    forbidden = set()
+    if do_not_sample:
+        with open(do_not_sample, encoding="utf-8") as input_stream:
+            for line in input_stream:
+                line = line.strip()
+                if line:
+                    forbidden.add(line)
 
     PN_tag = {"conllu": "PROPN", "wikiner": "NAM"}
     PN = PN_tag.get(corpus_format, "")
@@ -49,12 +65,15 @@ def main(infilename, corpus_format="guess", sample_size=1000, output_dir="."):
     elif corpus_format == "apil":
         corpus = apil.read_corpus(infilename)
     random.shuffle(corpus)
+    corpus = [sentence for sentence in corpus if sentence.id not in forbidden]
 
     n_toks = 0
     selected = []
     while n_toks < sample_size:
-        selected.append(corpus.pop())
-        n_toks += len(selected[-1])
+        sent = corpus.pop()
+        if min_size <= len(sent.text.split()) <= max_size:
+            selected.append(sent)
+            n_toks += len(selected[-1])
 
     basename = basename or infilepath.stem
     textfile = Path(output_dir) / (basename + ".sample.txt")
@@ -99,10 +118,28 @@ def parse_cl(argv=None):
         help="The size of the sample in number of tokens (default: %(default)s)."
     )
     parser.add_argument(
+        "-m", "--min-size",
+        type=int,
+        default=5,
+        help="The minimum size of a sentence (default: %(default)s)."
+    )
+    parser.add_argument(
+        "-x", "--max-size",
+        type=int,
+        default=60,
+        help="The maximum size of a sentence (default: %(default)s)."
+    )
+    parser.add_argument(
         "-o", "--output-dir",
         type=str,
         default=".",
         help="Output directory."
+    )
+    parser.add_argument(
+        "-d", "--do-not-sample",
+        metavar="FILE",
+        type=str,
+        help="A file containing a list of ids (one per line) that should not be sampled."
     )
     args = parser.parse_args(argv)
 
